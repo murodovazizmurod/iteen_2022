e4d71a6f25257d23dd3d34d79bd9d635638c7f29
Murodov Azizmurod <65248559+murodovazizmurod@users.noreply.github.com>
2024-02-12 00:44:26 +0500

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,17 +1,8 @@
-import logging
 from flask import Flask
 from backend.db import *
 
 
 app = Flask(__name__)
-
-# Logging configuration
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-handler = logging.FileHandler(filename='log/server.log')
-handler.setFormatter(formatter)
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
 
 # Connect views
 import backend.views
